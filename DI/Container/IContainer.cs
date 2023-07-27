a97e566239f89386a41a68a46f44278b36eb3608
Dmytrii Petrinko <dmytrii.petrinko@gmail.com>
2023-07-28 01:58:18 +0300

    Added Has, Flush, Restructured tests

--- DI/Container/IContainer.cs
+++ DI/Container/IContainer.cs
@@ -4,6 +4,12 @@ public interface IContainer
 {
 	void Add<T>(T obj, object id) where T : class;
 	void Add<T>(T obj) where T : class;
+
 	T Get<T>() where T : class;
 	T Get<T>(object id) where T : class;
+
+	bool Has<T>() where T : class;
+	bool Has<T>(object id) where T : class;
+
+	void Flush();
 }
