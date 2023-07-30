017106468037bd61ae00e68d26ae64f1087d63f4
Dmytrii Petrinko <dmytrii.petrinko@gmail.com>
2023-07-30 21:56:37 +0300

    Implemented DI base class

---
+++ DISystem/DI.cs
@@ -0,0 +1,8 @@
+﻿using DISystem.Context;
+
+namespace DISystem;
+
+public static class DI
+{
+	public static IContext RootContext { get; } = new Context.Implementation.Context();
+}
---
+++ DISystem/Tests/DITests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace DISystem.Tests;
+
+internal class DITests
+{
+	[Test]
+	public void RootContext_IsNotNull()
+	{
+		Assert.NotNull(DI.RootContext);
+	}
+}
